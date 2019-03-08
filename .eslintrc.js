a5fb28e8f9c281e661d3e9640135fc2c263eb3e4
Mohamed ElSaadany <mhsaadany@gmail.com>
2019-03-08 21:14:28 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es6": true,
+  },
+
+  "parserOptions": {
+    "ecmaVersion": 8
+  },
+
+  "extends": [
+    "eslint:recommended",
+    "google",
+  ],
+
+  "globals": {},
+
+  "plugins": [],
+
+  "rules": {},
+};
