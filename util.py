1ad2317fd533a73997f342d0ad04bb70f823531b
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-08-07 03:14:25 +0300

    show error when on non-windows OS

--- util.py
+++ util.py
@@ -1,3 +1,10 @@
+from click import echo, style
+
+
+def echo_error(message):
+    echo(f"[{style('error', fg='red')}] {message}", err=True)
+
+
 def quote_string(string):
     return '"' + string + '"'
 
