f4ef8f925b1d5db99274529d5cfbefb0cbb71b88
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-08-12 19:36:41 +0300

    add description, keywords & version to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup, find_packages
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='esg-cli',
     version='0.0.1',
+    description='Adds GOG & EGL games to Steam as shortcuts.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='epic, steam, galaxy, cli',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
         'click', 'legendary-gl', 'python-resize-image', 'requests', 'vdf'
     ],
